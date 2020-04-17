6d5036b4338787d366223d7d57edce78b1418f50
Cole Thomas Evans <cevans2@ualberta.ca>
2020-04-17 13:35:50 -0600

    initial update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Fragment } from 'react';
+import { Grid, Button } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface IAppProps {
 }
 
-export default App;
+export default class App extends React.Component<IAppProps> {
+  private incrementValueButtonClicked(): void {
+    console.log("increment called");
+    //this.props.actionCreatorForButtonIncrementValue
+  }
+
+  private decrementValueButtonClicked(): void {
+    console.log("decrement called");
+  }
+
+  private buttonTextColorChange(): void {
+    console.log("button text color change called");
+  }
+
+  public render() {
+    //Change color of a button using redux.
+    return (
+      <Fragment>
+        <Grid>
+          <Grid.Row centered>
+            <h1>Live Redux Counting App! (With Button Color Change)</h1>
+          </Grid.Row>
+          <Grid.Row centered>
+            <Grid.Row centered>
+              <Button
+                content="This Buttons Text Color Will Change!"
+                onClick={() => this.buttonTextColorChange()}
+              />
+            </Grid.Row>
+            <Grid.Row centered>
+              <h3>Current Count Value: 0</h3>
+            </Grid.Row>
+          </Grid.Row>
+          <Grid.Row centered>
+            <Button
+              content="Increment Value"
+              onClick={() => this.incrementValueButtonClicked()}
+              color="facebook"
+            />
+            <Button
+              content="Decrement Value"
+              onClick={() => this.decrementValueButtonClicked()}
+              color="linkedin"
+            />
+          </Grid.Row>
+        </Grid>
+
+      </Fragment>
+    );
+  }
+}
